e960e17d129e84d4d9ac20ec99712d5dc039722e
kevin park <kevin.park02@gmail.com>
2020-11-23 14:36:25 -0800

    Create a rootfile where all components will be compiled

--- widgets/frontend/widgets.jsx
+++ widgets/frontend/widgets.jsx
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './root';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const main = document.getElementById('main');
+
+    ReactDOM.render(<Root />, main);
+})
